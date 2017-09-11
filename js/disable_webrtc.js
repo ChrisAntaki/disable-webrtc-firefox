76d5228e452d1ab43ade20c6af2fd4fe8835347f
Chris Antaki <ChrisAntaki@gmail.com>
2017-09-10 22:55:41 -0700

    Migrating to WebExtension

---
+++ js/disable_webrtc.js
@@ -0,0 +1,34 @@
+// Disable WebRTC by default.
+let isEnabled = false;
+toggleWebRTC(isEnabled);
+
+// Handle clicks on the action button.
+browser.browserAction.onClicked.addListener(handleClick);
+
+/**
+ * Toggles WebRTC on and off.
+ * @param {boolean=} enable
+ */
+function toggleWebRTC(enable = true) {
+  // Update setting.
+  browser.privacy.network.peerConnectionEnabled.set({ value: enable });
+  browser.privacy.network.webRTCIPHandlingPolicy.set(
+    { value: enable ? 'default' : 'disable_non_proxied_udp' });
+
+  // Update title.
+  const title = enable ?
+    'WebRTC is not disabled. Be careful.' :
+    'WebRTC is disabled.';
+  browser.browserAction.setTitle({ title });
+
+  // Update icon.
+  const name = enable ? 'unsafe' : 'safe';
+  const path = `images/${name}-48.png`;
+  browser.browserAction.setIcon({ path });
+}
+
+/** Handles clicks on the action button. */
+function handleClick(e) {
+  isEnabled = !isEnabled;
+  toggleWebRTC(isEnabled);
+}
